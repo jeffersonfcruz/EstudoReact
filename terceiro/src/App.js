ab95968feecff74bc5dd67e5936d5346715b3d93
Jefferson Cruz <jeffcruz1998@hotmail.com>
2022-05-12 15:32:39 -0300

    Aprendendo sobre estado

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,19 +1,14 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let valor1 = Math.round(Math.random() * 15);
-  let valor2 = Math.round(Math.random() * 15);
-
+  const [numero, setNumero] = useState(1);
   return (
     <div>
-      <p>A Soma de 50 e 10 é {50 + 10}</p>
-      <p>O resto da divisão entre 50 e 3 é {50 % 3}</p>
+      <p>O numero atual é {numero}</p>
       <p>
-        A soma entre {valor1} e {valor2} é {valor1 + valor2}
-      </p>
-      <p>
-        O resultado da soma acima é um numero{" "}
-        {(valor1 + valor2) % 2 === 0 ? "Par" : "Impar"}
+        <button title="Clique" onClick={() => setNumero(numero + 1)}>
+          Clique Aqui
+        </button>
       </p>
     </div>
   );
